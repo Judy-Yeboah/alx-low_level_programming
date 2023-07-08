a3297b676748eb95eb2ed29e9ee5e96b62129409
Judy-Yeboah <jyeboah513@gmail.com>
2023-07-08 00:50:22 +0000

    Static Lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
-void _puts(char *str)
 #include<stdio.h>
 /**
+ * _puts - Prints a string to the standard output
+ * @str: The string to be printed
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
