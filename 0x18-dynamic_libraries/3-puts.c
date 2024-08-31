713808ea0664352cb2a7a6278dc2bea61fe705d2
Judy-Yeboah <jyeboah513@gmail.com>
2024-08-31 04:42:31 +0000

    dynamic libraries

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts - Prints a string to the standard output
+ * @str: The string to be printed
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
